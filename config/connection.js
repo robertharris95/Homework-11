98a189e6e8b9039efa68aedb74c3ea799e811654
Robert Harris <rlh9@rice.edu>
2020-03-13 18:28:43 -0500

    <heroku>

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: process.env.PORT || 3306,
-  user: "root",
-  password: "Rh12271995",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  onnection = mysql.createConnection({
+    host: "localhost",
+    port: process.env.PORT || 3306,
+    user: "root",
+    password: "Rh12271995",
+    database: "burgers_db"
+  });
+}
 
 // Make connect!
 connection.connect(function(err) {
